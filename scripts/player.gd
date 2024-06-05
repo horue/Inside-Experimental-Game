a25c524281ddedd1219e18a881d7e8860497260a
Jorge <jorgejapao00@gmail.com>
2024-06-04 21:58:57 -0300

    The player's sprite now flips when moving in different directions

--- scripts/player.gd
+++ scripts/player.gd
@@ -19,5 +19,9 @@ func _process(delta):
 		$AnimatedSprite2D.play()
 	else:
 		$AnimatedSprite2D.stop()
+	if velocity.x < 0:
+		$AnimatedSprite2D.flip_h = true
+	if velocity.x > 0:
+		$AnimatedSprite2D.flip_h = false
 	position += velocity * delta
 	position = position.clamp(Vector2.ZERO, screen_size)

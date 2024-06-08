31b8864a5339a0334897da798bfb021975aa97e4
Jorge <jorgejapao00@gmail.com>
2024-06-07 23:26:52 -0300

    Player script updated to play animations

--- scripts/player.gd
+++ scripts/player.gd
@@ -2,13 +2,19 @@ extends Area2D
 signal hit
 
 
-@export var speed = 300
+@export var speed = 100
 var screen_size
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	screen_size = get_viewport_rect().size
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	if Input.is_action_pressed('dance_button'):
+		$AnimatedSprite2D.play('dance')
+		return
+	if Input.is_action_pressed('dance2_button'):
+		$AnimatedSprite2D.play('dance2')
+		return
 	var velocity = Vector2.ZERO # The player's movement vector.
 	if Input.is_action_pressed('move_left'):
 		velocity.x -=1
@@ -16,17 +22,22 @@ func _process(delta):
 		velocity.x +=1
 	if Input.is_action_pressed('jump'):
 		velocity.y -=2
+		$AnimatedSprite2D.play('jump')
+		return
 	if velocity.length() > 0:
 		velocity = velocity.normalized() * speed
-		$AnimatedSprite2D.play()
+		$AnimatedSprite2D.play("walk")
 	else:
-		$AnimatedSprite2D.stop()
+		$AnimatedSprite2D.play("idle")
 	if velocity.x < 0:
 		$AnimatedSprite2D.flip_h = true
 	if velocity.x > 0:
 		$AnimatedSprite2D.flip_h = false
+
 	position += velocity * delta
 	position = position.clamp(Vector2.ZERO, screen_size)
+	
+
 
 
 func _on_body_entered(body):
